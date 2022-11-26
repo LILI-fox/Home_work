1efae8c74827cd1a81cfed5271bb3a89f63b0dea
Lilith <ll.file.top.secret@yandex.ru>
2022-11-27 01:08:23 +0300

    add Task_05 - Seminar_2_task_1

---
+++ Task_05/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 10. Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5; 782 -> 8; 918 -> 1
+
+Console.Write("Введите целое трехзначное число: ");
+bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+
+if (!isParsed)
+{
+    Console.WriteLine("Вы ввели не целое число. Введите другое число!");
+    return;
+}
+if (number < 0 | number >= 1000)
+{
+    Console.WriteLine("Вы ввели не трехзначное число. Введите другое число!");
+}
+
+    int FSnumber = number / 10;
+    int secondNumber = FSnumber % 10;
+
+    Console.WriteLine($"Вторая цифра числа {number} - {secondNumber}");
