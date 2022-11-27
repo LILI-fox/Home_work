098a10a1cf23e7d9ec01e38ca3786ed868e30ff8
Lilith <ll.file.top.secret@yandex.ru>
2022-11-27 22:07:36 +0300

    add Task_07 - Seminar_2_task_3

---
+++ Task_07/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5; 78 -> третьей цифры нет; 32679 -> 6
+
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = number.ToString().Length;
+
+int array(int arg1, int arg2)
+{
+    int result = 0;
+    if(arg2>=3)
+        {
+        int numberC = 1;
+        for (int i = arg2; i > 3; i--)
+            {
+                numberC = numberC * 10;
+            }
+        result = (arg1 / numberC) % 10;
+        }
+    return result;
+}
+
+Console.WriteLine($"Третья цифра числа {number} равна {array(number, count)}"); 
+
+if (count <= 2)
+{
+    Console.WriteLine($"В числе {number} нет третьей цифры. Цифр в числе: {count}. Введите трехзначное число!");
+}
