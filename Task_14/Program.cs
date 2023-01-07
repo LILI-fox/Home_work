03cdad84239dc2b7c1e7ac05ad76d2087362465e
Lilith <ll.file.top.secret@yandex.ru>
2023-01-07 21:33:50 +0300

    add Task_14 - Seminar_5_task_1

---
+++ Task_14/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] array = GenerateArray(100, 999, 10);
+
+PrintArray(array);
+
+int[] resultArray = GetPositiveNumbers(array);
+
+PrintArray(resultArray);
+
+int[] GenerateArray(int min, int max, int lenght)
+{
+    Random random = new Random();
+    int[] array = new int[lenght];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+int[] GetPositiveNumbers(int[] array)
+{
+    int[] newArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            newArray[i] = array[i];
+        }
+    }
+    return newArray;
+}
