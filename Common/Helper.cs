40e3250e2d88c68d92172689082f632785580f2e
Lilith <ll.file.top.secret@yandex.ru>
2023-01-09 03:22:37 +0300

    add add Task_17 - Seminar_6_task_1

---
+++ Common/Helper.cs
@@ -0,0 +1,32 @@
+﻿namespace Common;
+
+public static class Helper
+{
+    public static void PrintArray(int[] array)
+                {
+                    string message = string.Join(", ", array);
+                    Console.WriteLine($"[{message}]");
+                }
+    public static int[] GenerateArray(int min, int max, int lenght)
+                {
+                    Random random = new Random();
+                    int[] array = new int[lenght];
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                      array[i] = random.Next(min, max + 1);
+                    }
+                return array;
+                }
+    public static int InputNumber()
+    {
+        bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+        if(isParsed)
+        {
+            return number;
+        }
+        else
+        {
+            throw new Exception("Введите корректное число!");
+        }
+    }
+}
---
+++ Task_17/Program.cs
@@ -0,0 +1,32 @@
+﻿using static Common.Helper;
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, -223-> 3
+
+Console.WriteLine("Введите количество чисел: ");
+
+int number = int.Parse(Console.ReadLine());
+
+int[] array = GenerateArray(-1000, 1000, number);
+
+PrintArray(array);
+
+int result = GetPositiveNum(array);
+
+Console.WriteLine($"Количество положительных чисел равно {result}.");
+
+int GetPositiveNum(int[] array)
+{
+    int[] newArr = new int[array.Length];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
